4217b1cd042e480e12dec35f2a77a75051faefe3
Berkhz <kauan.hb2004@gmail.com>
2022-07-08 17:12:52 -0300

    update script

--- gerador-senha/script.js
+++ gerador-senha/script.js
@@ -1,1 +1,16 @@
-console.log("TESTE")
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
